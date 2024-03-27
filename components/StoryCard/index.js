a076650294c99d4390694e1d6da986e7798f44ca
thefigtree <ldj8431@naver.com>
2024-03-27 16:18:13 +0900

    Main Dev

--- components/StoryCard/index.js
+++ components/StoryCard/index.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import Styles from "./story_card.module.css";
 
 export default class StoryCard extends Component {
@@ -9,25 +9,26 @@ export default class StoryCard extends Component {
 
   render() {
     return (
-      <Link href={this.props.item.date}>
-        <div
-          className={Styles.container}
-          style={{
-            backgroundImage: `url(${this.props.item.cover})`,
-          }}
-          onMouseEnter={this.props.flyTo}
-          onTouchStart={this.props.flyTo}
-          onMouseOut={this.props.zoomOut}
-          onTouchEnd={this.props.zoomOut}
-          data-lat={this.props.item.lat}
-          data-lng={this.props.item.lng}
-        >
-          <div className={Styles.overlay}>
-            <div className={Styles.date}>{this.props.item.date}</div>
-            <div className={Styles.title}>{this.props.item.title}</div>
-          </div>
+      // <Link href={this.props.item.date}>
+
+      // </Link>
+      <div
+        className={Styles.container}
+        style={{
+          backgroundImage: `url(${this.props.item.cover})`,
+        }}
+        onMouseEnter={this.props.flyTo}
+        onTouchStart={this.props.flyTo}
+        onMouseOut={this.props.zoomOut}
+        onTouchEnd={this.props.zoomOut}
+        data-lat={this.props.item.lat}
+        data-lng={this.props.item.lng}
+      >
+        <div className={Styles.overlay}>
+          <div className={Styles.date}>{this.props.item.date}</div>
+          <div className={Styles.title}>{this.props.item.title}</div>
         </div>
-      </Link>
+      </div>
     );
   }
 }
